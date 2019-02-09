2133564b04c7da7360aa871a2b1606b31436be9d
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-09 12:12:04 -0800

    lab7

--- DynIntQueue.cpp
+++ DynIntQueue.cpp
@@ -1,5 +1,60 @@
 //
 // Created by Sandro on 2/9/2019.
 //
-
 #include "DynIntQueue.hpp"
+
+DynIntQueue::DynIntQueue() {
+    front = nullptr;
+}
+
+DynIntQueue::~DynIntQueue() {
+    QueueNode *garbage = front;
+    while (garbage != nullptr) {
+        front = front->next;
+        garbage->next = nullptr;
+        delete garbage;
+        garbage = front;
+    }
+}
+
+void DynIntQueue::enqueue(int num) {
+    if (isEmpty()) {
+        front = new QueueNode(num);
+        rear = front;
+    }
+    else {
+        rear->next = new QueueNode(num);
+        rear = rear->next;
+    }
+}
+
+void DynIntQueue::dequeue(int &num) {
+    QueueNode *temp = nullptr;
+    if (isEmpty()) {
+        cout << "The queue is empty.\n";
+        exit(1);
+    }
+    else {
+        num = front->value;
+        temp = front;
+        front = front->next;
+        delete temp;
+    }
+}
+
+bool DynIntQueue::isEmpty() const {
+    if (front == nullptr) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void DynIntQueue::clear() {
+    int value;
+
+    while (!isEmpty()) {
+        dequeue(value);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include "DynIntQueue.hpp"
 
 int main() {
+    DynIntQueue iQueue;
+    cout << "Enqueuing 5 items...\n";
 
+    // enqueue 5 items
+    for (int k = 1; k < 5; k++) {
+        iQueue.enqueue(k*k);
+    }
 
-
+    // dequeue and retrieve all items in the queue
+    cout << "The values in the queue were:\n";
+    while (!iQueue.isEmpty()) {
+        int value;
+        iQueue.dequeue(value);
+        cout << value << " ";
+    }
 
     return 0;
 }
