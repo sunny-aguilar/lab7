be5e1a891bf3cb2b80cc2451d6f0bc2d5d16d0a2
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-09 11:55:08 -0800

    lab7

---
+++ DynIntQueue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sandro on 2/9/2019.
+//
+
+#include "DynIntQueue.hpp"
---
+++ DynIntQueue.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Sandro on 2/9/2019.
+//
+
+#ifndef LAB7_DYNINTQUEUE_HPP
+#define LAB7_DYNINTQUEUE_HPP
+
+#endif //LAB7_DYNINTQUEUE_HPP
