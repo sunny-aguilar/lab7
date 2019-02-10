f3acce8292673df52a45de685c6329426fe74980
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-09 21:28:29 -0800

    lab7

---
+++ menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sandro Aguilar on 2019-02-09.
+//
+
+#include "menu.hpp"
---
+++ menu.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Sandro Aguilar on 2019-02-09.
+//
+
+#ifndef LAB7_MENU_HPP
+#define LAB7_MENU_HPP
+
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Menu {
+public:
+
+private:
+
+};
+
+
+#endif //LAB7_MENU_HPP
