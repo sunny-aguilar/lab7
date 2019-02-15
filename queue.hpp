19e121647f54f65dffb153c21a9daa6bfade6cd9
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-14 20:10:59 -0800

    lab7

--- queue.hpp
+++ queue.hpp
@@ -4,7 +4,7 @@
 ** Date: 				02/20/2019
 ** Description:         Queue class implements a circular linked list
 **                      where the tail pointer points back to the
-**                      front of the list.
+**                      front of the list. Node is declared as a
 *********************************************************************/
 #ifndef LAB7_QUEUE_HPP
 #define LAB7_QUEUE_HPP
