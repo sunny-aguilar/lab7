068e5ef4cb50d9265a464eae78c4ca86742e0660
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-14 20:07:48 -0800

    lab7

--- queue.hpp
+++ queue.hpp
@@ -2,8 +2,8 @@
 ** Program name: 		LAB 7 - Linked List
 ** Author: 				Sandro Aguilar
 ** Date: 				02/20/2019
-** Description:         Menu class provides the driver program to
-**                      interact with the queue list.
+** Description:         Queue class implements a circular linked list
+**                      interact with the queue list. Menu
 *********************************************************************/
 #ifndef LAB7_QUEUE_HPP
 #define LAB7_QUEUE_HPP
