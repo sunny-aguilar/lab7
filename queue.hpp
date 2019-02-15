31666e06ccbfb4efcec562ff868203adc8c24349
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-14 20:04:38 -0800

    lab7

--- queue.hpp
+++ queue.hpp
@@ -2,7 +2,8 @@
 ** Program name: 		LAB 7 - Linked List
 ** Author: 				Sandro Aguilar
 ** Date: 				02/20/2019
-** Description:         Menu class description here
+** Description:         Menu class provides the driver program to
+ *                      interact with the
 *********************************************************************/
 #ifndef LAB7_QUEUE_HPP
 #define LAB7_QUEUE_HPP
