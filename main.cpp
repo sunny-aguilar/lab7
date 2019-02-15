fdbc8cd086c5696caa372849e0f88be299038928
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-14 20:39:51 -0800

    lab7

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 ** Date: 				02/20/2019
 ** Description:         This program creates an example of a circular
 **                      linked list. The nodes are created using a
-**                      struct container that holds a pointer
+**                      struct container that holds an integer
 *********************************************************************/
 #include <iostream>
 #include "queue.hpp"
